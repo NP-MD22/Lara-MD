ff1cbb968298c70b6fb645dffa517a99b472008a
NP-MD22 <Nethsarapumudi2009@gmail.com>
2024-12-23 08:50:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "fmgEUaAK#3GN--zMLjVrPgptCgdsaDg35UerWRI1DUt-7Uwll00w", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
